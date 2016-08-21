4468604f3380e41095ee1e57073d45e07f38344b
Keitel Jovin <keiteldog@gmail.com>
2016-08-21 14:00:47 -0500

    create login form with onclick methods

--- app/src/main/java/com/megalobiz/megalobiz/activities/LoginActivity.java
+++ app/src/main/java/com/megalobiz/megalobiz/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package com.megalobiz.megalobiz.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.megalobiz.megalobiz.R;
 
@@ -11,5 +12,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // Click handler method for the button used to start OAuth flow
+    // Uses the client to initiate OAuth authorization
+    // This should be tied to a button used to login
+    public void loginToRest(View view) {
+
+    }
+
+    // when click Home Screen icon or text, client will connect
+    // with client credentials grant_type
+    public void onHomeScreen(View view) {
+
+    }
+
+    // go to register activity
+    public void onRegister(View view) {
+
     }
 }
