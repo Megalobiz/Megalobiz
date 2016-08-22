3cd444e51a72cf4ec05c067a2f56c94fbdad0b0a
Keitel Jovin <keiteldog@gmail.com>
2016-08-22 15:04:48 -0500

    Put the Hamburger implementation in a shared class to be reusable in other activitie

--- app/src/main/java/com/megalobiz/megalobiz/activities/TopShowbizActivity.java
+++ app/src/main/java/com/megalobiz/megalobiz/activities/TopShowbizActivity.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 import com.megalobiz.megalobiz.MegalobizApplication;
 import com.megalobiz.megalobiz.MegalobizClient;
 import com.megalobiz.megalobiz.R;
+import com.megalobiz.megalobiz.activities.helpers.SharedHamburger;
 import com.megalobiz.megalobiz.activities.helpers.SharedMenu;
 import com.megalobiz.megalobiz.adapters.HamburgerArrayAdapter;
 import com.megalobiz.megalobiz.utils.HamburgerItem;
@@ -27,12 +28,6 @@ import java.util.ArrayList;
 import java.util.zip.Inflater;
 
 public class TopShowbizActivity extends AppCompatActivity {
-
-    private HamburgerArrayAdapter hamAdapter;
-    private ArrayList<HamburgerItem> hamItems;
-    private ListView lvHamburger;
-    private DrawerLayout dlHamburger;
-    private ActionBarDrawerToggle drawerToggle;
 
     MegalobizClient client;
 
@@ -46,52 +41,12 @@ public class TopShowbizActivity extends AppCompatActivity {
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setDisplayUseLogoEnabled(true);
         ab.setLogo(R.drawable.ic_megalobiz);
-        ab.setTitle("Megalo Hits");
+        ab.setTitle(R.string.drawer_home_close);
 
         client = MegalobizApplication.getRestClient();
 
-        hamItems = new ArrayList<>();
-        hamItems.add(new HamburgerItem("Megalo Hits", R.drawable.mb_icon_home_white));
-        hamItems.add(new HamburgerItem("Bands", R.drawable.mb_icon_band_white));
-        hamItems.add(new HamburgerItem("Musicians", R.drawable.mb_icon_musician_white));
-        hamItems.add(new HamburgerItem("Albums", R.drawable.mb_icon_album_white));
-        hamItems.add(new HamburgerItem("Songs", R.drawable.mb_icon_song_white));
-        hamItems.add(new HamburgerItem("About", R.drawable.mb_icon_about_white));
-
-        // DrawerLayout
-        dlHamburger = (DrawerLayout) findViewById(R.id.dlHamburger);
-
-        // Populate the Navigtion Drawer with options
-        //mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
-        lvHamburger = (ListView) findViewById(R.id.lvHamburger);
-        hamAdapter = new HamburgerArrayAdapter(this, hamItems);
-        lvHamburger.setAdapter(hamAdapter);
-
-        // Drawer Item click listeners
-        lvHamburger.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                goToActivity(position);
-            }
-        });
-
-        drawerToggle = new ActionBarDrawerToggle(this, dlHamburger, R.string.drawer_open, R.string.drawer_home_close) {
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-
-                invalidateOptionsMenu();
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-
-                invalidateOptionsMenu();
-            }
-        };
-
-        dlHamburger.setDrawerListener(drawerToggle);
+        // set the Hamburger menu with shared static class
+        SharedHamburger.cookHamburger(this);
 
         Toast.makeText(this,
                 "Connected to API with "+ MegalobizApplication.grantType, Toast.LENGTH_LONG).show();
@@ -105,23 +60,7 @@ public class TopShowbizActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
-    public void goToActivity(int position) {
-        if (position == 0) {
-            Toast.makeText(this, "Top Showbiz Activity", Toast.LENGTH_SHORT).show();
-        } else if (position == 1) {
-            Toast.makeText(this, "Band Showbizs Activity", Toast.LENGTH_SHORT).show();
-        } else if (position == 2) {
-            Toast.makeText(this, "Musician Showbizs Activity", Toast.LENGTH_SHORT).show();
-        } else if (position == 3) {
-            Toast.makeText(this, "Album Showbizs Activity", Toast.LENGTH_SHORT).show();
-        } else if (position == 4) {
-            Toast.makeText(this, "Song Showbizs Activity", Toast.LENGTH_SHORT).show();
-        } else if (position == 5) {
-            Toast.makeText(this, "About Showbizs Activity", Toast.LENGTH_SHORT).show();
-        }
+        SharedHamburger.drawerToggle.syncState();
     }
 
     @Override
@@ -143,7 +82,7 @@ public class TopShowbizActivity extends AppCompatActivity {
         // Pass the event to ActionBarDrawerToggle
         // If it returns true, then it has handled
         // the nav drawer indicator touch event
-        if (drawerToggle.onOptionsItemSelected(item)) {
+        if (SharedHamburger.drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
