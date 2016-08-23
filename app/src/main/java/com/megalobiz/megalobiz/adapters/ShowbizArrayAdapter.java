a13cb8fcbe9f7396878036e758baf7c3f5b78ca2
Keitel Jovin <keiteldog@gmail.com>
2016-08-23 12:45:27 -0500

    Implement Showbiz models listing with Endless Scrolling

--- app/src/main/java/com/megalobiz/megalobiz/adapters/ShowbizArrayAdapter.java
+++ app/src/main/java/com/megalobiz/megalobiz/adapters/ShowbizArrayAdapter.java
@@ -1,14 +1,23 @@
 package com.megalobiz.megalobiz.adapters;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.megalobiz.megalobiz.R;
 import com.megalobiz.megalobiz.activities.helpers.SharedHamburger;
 import com.megalobiz.megalobiz.models.Showbiz;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 /**
  * Created by KeitelRobespierre on 8/22/2016.
@@ -17,5 +26,35 @@ public class ShowbizArrayAdapter extends ArrayAdapter<Showbiz> {
 
     public ShowbizArrayAdapter(Context context, List<Showbiz> showbizs) {
         super(context, android.R.layout.simple_list_item_1, showbizs);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // get the Showbiz
+        Showbiz showbiz = getItem(position);
+
+        // find or inflate the template
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.showbiz_item, parent, false);
+        }
+
+        ImageView ivProfilePicture = (ImageView) convertView.findViewById(R.id.ivProfilePicture);
+        ivProfilePicture.setImageResource(0);
+
+        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+        tvName.setText(showbiz.getName());
+
+        // set the images with Picasso
+        // set profile image
+        String profileUrl = showbiz.getSmallProfilePicture();
+        if(!TextUtils.isEmpty(profileUrl)) {
+            Picasso.with(getContext()).load(profileUrl)
+                    .transform(new RoundedCornersTransformation(3, 3))
+                    .into(ivProfilePicture);
+        }
+
+
+        return convertView;
     }
 }
---
+++ app/src/main/java/com/megalobiz/megalobiz/utils/NetworkState.java
@@ -0,0 +1,59 @@
+package com.megalobiz.megalobiz.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+
+/**
+ * Created by KeitelRobespierre on 8/23/2016.
+ */
+public class NetworkState {
+
+    private Context context;
+    private Activity activity;
+
+    public NetworkState(Context context) {
+        this.context = context;
+        this.activity = (Activity) context;
+    }
+
+    public Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    public static boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+        return false;
+    }
+
+    public void closeIfNoConnection() {
+        if (! isNetworkAvailable() || !isOnline()) {
+
+            Toast.makeText(activity, "Please Check your Internet Connection!", Toast.LENGTH_LONG).show();
+
+            // end this activity after a while
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    activity.finish();
+                }
+            }, 3000);
+        }
+    }
+
+}
